96378719cdd3d2ebe74bed5fb7b4ad7c962b9b62
Subekti170504 <112887737+Subekti170504@users.noreply.github.com>
2022-09-06 12:57:26 +0700

    Add files via upload

---
+++ Tugas 1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Nama         : Subekti Wahyu Aji" << endl;
+    cout << "NIM          : A11.2022.14627" << endl;
+    cout << "Asal Sekolah : SMK N 2 Semarang" << endl;
+    return 0;
+}
