c566c68054352523ac3b1dd8b8baa1afbd1cabdb
Subekti170504 <112887737+Subekti170504@users.noreply.github.com>
2022-09-12 20:13:19 +0700

    Add files via upload

---
+++ Pertemuan 3/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int x = 2000;
+    cout << "Hello world!" << x;
+    cout << endl;
+    //printf("Test");
+
+    int u = 18;
+    float t = 163.5;
+    char b = 'XXL';
+    string ub = "XXL";
+
+    cout << "Umur saya : " << u;
+    cout << "\nTinggi saya : " << t;
+    cout << "\nUkuran baju : " << ub;
+    return 0;
+}
