3ae88a4488b702edfdadd5790e4e51164ab54eb2
Subekti170504 <112887737+Subekti170504@users.noreply.github.com>
2022-09-20 19:26:17 +0700

    Add files via upload

---
+++ pertemuan 5/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Menghitung Luas Lingkaran" << endl;
+    cout << "\nSubekti Wahyu Aji \nA11.2022.14627 \n4117 \n" << endl;
+
+    float r;
+    float pi;
+
+    pi = 3.14;
+
+    cout << "Masukan jari-jari lingkaran: ";
+    cin >> r;
+    cout << "L = pi x r^2 \nL =" << pi*r*r << endl;
+    cout << "Lua lingkaran =" << pi*r*r << endl;
+    return 0;
+}
