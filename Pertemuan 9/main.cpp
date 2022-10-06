d474f5ee791fd87d4595ae12f1dacad28484abe2
Subekti170504 <112887737+Subekti170504@users.noreply.github.com>
2022-10-06 09:35:35 +0700

    Add files via upload

---
+++ Pertemuan 9/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Tugas Pertemuan 9" << endl;
+    cout << "Mencari Angka Kelipatan 5" << endl;
+
+    int a;
+
+    a = 5;
+    while (a <= 100){
+        cout << a << endl;
+        a+=5;
+    }
+    return 0;
+}
